6a31e4f7eed66904d2738cb5b2ad54f83d33821f
Dorian Thompson <dorianthompson2@Dorians-MBP.fios-router.home>
2023-10-17 08:34:03 -0400

    Add Boilerplate for ReactNative and screens for the app

---
+++ tracks/App.js
@@ -0,0 +1,6 @@
+import SignupScreen from "./src/screens/SignUpScreen";
+import SigninScreen from "./src/screens/SignInScreen";
+import AccountScreen from "./src/screens/AccountScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
